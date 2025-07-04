375b0d85c75d44feea5812e0cf0480ffbff40dd0
atakamori <takamori@eri.u-tokyo.ac.jp>
2025-07-04 10:25:51 +0900

    [modified]
    標準出力のバッファオーバーフローを起こしていたので、処理経過をターミナルに表示する頻度を落とす。
    これまでは、年月日時分とデータディレクトリ、フィット長を表示していた。
    年月日とデータディレクトリのみに変更した。

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,5 @@
+// データ読み込み用配列を確保
+	id0=calloc(60*SAMP,sizeof(int)); if (id0==NULL)exit(0);
+	id1=calloc(60*SAMP,sizeof(int)); if (id1==NULL)exit(0);
+	id2=calloc(60*SAMP,sizeof(int)); if (id2==NULL)exit(0);
+	id3=calloc(60*SAMP,sizeof(int)); if (id3==NULL)exit(0);
